e6d799d2d02cb7d40ecc36ce954def06f68e2092
ccsplit <cshay237@gmail.com>
2019-03-25 09:03:26 -0500

    Add gosrc alias and an antigen package for gobuffalo/buffalo

--- zsh/go.zsh
+++ zsh/go.zsh
@@ -1,2 +1,5 @@
 export GOPATH=$HOME/Code/go/workspace
 export PATH="$PATH:$GOPATH/bin"
+alias gosrc="cd $GOPATH/src/github.com/$(git config --includes --get user.name)"
+
+antigen bundle 1995parham/buffalo.zsh
