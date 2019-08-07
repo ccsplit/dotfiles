23b6ae8bc49532826b46dc789aa3e495f3ce561a
ccsplit <cshay237@gmail.com>
2019-08-07 14:48:19 -0500

    Rust changes

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,4 +1,4 @@
 # Environment variables
-export PATH=$HOME/bin:$PATH:"$HOME/Code/depot_tools"
+export PATH=$HOME/bin:$PATH:"$HOME/Code/depot_tools":$HOME/.cargo/bin
 export CDPATH=$HOME:"$HOME/Code":/etc:/var
 export JAVA_ENCODING="UTF-8"
